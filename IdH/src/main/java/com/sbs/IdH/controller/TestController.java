9900fe7227a255fd6988c73e537b6a349b1aa2c0
ewqweqewqqqq <gjalsgh000@naver.com>
2023-03-02 18:13:31 +0900

    test view commit

--- IdH/src/main/java/com/sbs/IdH/controller/TestController.java
+++ IdH/src/main/java/com/sbs/IdH/controller/TestController.java
@@ -6,6 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
+	@GetMapping("/issue")
+	public void issue() throws Exception {}
+	@GetMapping("/notice")
+	public void notice() throws Exception {}
+	@GetMapping("/mywork")
+	public void mywork() throws Exception {}
 	@GetMapping("/test")
 	public void test() throws Exception {}
+	@GetMapping("/teamone")
+	public void teamone() throws Exception {}
 }
