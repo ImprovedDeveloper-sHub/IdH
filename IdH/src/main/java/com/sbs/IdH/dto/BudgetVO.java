6f48aea52a3e1487515e885ba3487183969d49df
seoseunghoon <seoseunghoon>
2023-03-05 01:01:12 +0900

    budget init

---
+++ IdH/src/main/java/com/sbs/IdH/dto/BudgetVO.java
@@ -0,0 +1,13 @@
+package com.sbs.IdH.dto;
+
+public class BudgetVO {
+
+	private int budget_number;
+	private int budget_status;
+	private String budget_detail;
+	private int budget_price;
+	private int project_number;
+	private String budget_member_id;
+	
+	
+}
