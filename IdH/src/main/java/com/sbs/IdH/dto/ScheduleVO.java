1777d4b98a12d249c1268d5ec7830992ce42e97b
seoseunghoon <seoseunghoon>
2023-03-05 22:00:27 +0900

    schedule test

--- IdH/src/main/java/com/sbs/IdH/dto/ScheduleVO.java
+++ IdH/src/main/java/com/sbs/IdH/dto/ScheduleVO.java
@@ -6,24 +6,16 @@ public class ScheduleVO {
 
 	private int schedule_number;
 	private int schedule_status;
+	private String schedule_member_id;
+	
+	private int schedule_project_number;
+	
+	
+	private String schedule_detail;
 	private Date schedule_regdate;
 	private Date schedule_enddate;
-	private int schedule_project_number;
-	private int schedule_member_id;
 	
 	
-	
-	
-	public ScheduleVO(int schedule_number, int schedule_status, Date schedule_regdate, Date schedule_enddate,
-			int schedule_project_number, int schedule_member_id) {
-		super();
-		this.schedule_number = schedule_number;
-		this.schedule_status = schedule_status;
-		this.schedule_regdate = schedule_regdate;
-		this.schedule_enddate = schedule_enddate;
-		this.schedule_project_number = schedule_project_number;
-		this.schedule_member_id = schedule_member_id;
-	}
 	public int getSchedule_number() {
 		return schedule_number;
 	}
@@ -54,19 +46,32 @@ public class ScheduleVO {
 	public void setSchedule_project_number(int schedule_project_number) {
 		this.schedule_project_number = schedule_project_number;
 	}
-	public int getSchedule_member_id() {
+	public String getSchedule_member_id() {
 		return schedule_member_id;
 	}
-	public void setSchedule_member_id(int schedule_member_id) {
+	public void setSchedule_member_id(String schedule_member_id) {
 		this.schedule_member_id = schedule_member_id;
 	}
-	@Override
-	public String toString() {
-		return "ScheduleVO [schedule_number=" + schedule_number + ", schedule_status=" + schedule_status
-				+ ", schedule_regdate=" + schedule_regdate + ", schedule_enddate=" + schedule_enddate
-				+ ", schedule_project_number=" + schedule_project_number + ", schedule_member_id=" + schedule_member_id
-				+ "]";
+	public String getSchedule_detail() {
+		return schedule_detail;
 	}
+	public void setSchedule_detail(String schedule_detail) {
+		this.schedule_detail = schedule_detail;
+	}
+	public ScheduleVO(int schedule_number, int schedule_status, String schedule_member_id, int schedule_project_number,
+			String schedule_detail, Date schedule_regdate, Date schedule_enddate) {
+		super();
+		this.schedule_number = schedule_number;
+		this.schedule_status = schedule_status;
+		this.schedule_member_id = schedule_member_id;
+		this.schedule_project_number = schedule_project_number;
+		this.schedule_detail = schedule_detail;
+		this.schedule_regdate = schedule_regdate;
+		this.schedule_enddate = schedule_enddate;
+	}
+	
+	
+	
 	
 	
 	
