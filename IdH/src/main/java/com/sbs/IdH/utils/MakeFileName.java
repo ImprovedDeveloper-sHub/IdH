ff96bc0e7978d2063be857360e2ba2e3ab19213f
seoseunghoon <ytur13@naver.com>
2023-03-12 22:16:22 +0900

    makefileName

---
+++ IdH/src/main/java/com/sbs/IdH/utils/MakeFileName.java
@@ -0,0 +1,11 @@
+package com.sbs.IdH.utils;
+
+public class MakeFileName {
+
+	public static String toUUIDFileName(String originalFilename, String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
